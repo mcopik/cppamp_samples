221e8fa28c0f10674a397bd227166113762400f1
Marcin Copik <mcopik@gmail.com>
2016-06-28 17:49:58 -0500

    AMP reader with array_view

---
+++ amp/data_read.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cstdlib>
+
+#include <amp.h>
+
+namespace amp = Concurrency;
+template<typename data_type>
+struct view_accessor
+{
+
+    view_accessor(amp::array<data_type> & data) : data(data), data_view(data)
+    {}
+
+    void read_write(const uint32_t pos, data_type val)
+    {
+        write(pos, val);
+        double new_val = read(pos);
+        assert( val == new_val );
+    }
+
+    void write(const uint32_t pos, data_type val)
+    {
+        data_view[pos] = val;
+        data_view.synchronize();
+    }
+
+    data_type read(const uint32_t pos)
+    {
+        return data_view[pos];
+    }
+
+private:
+    amp::array<data_type> & data;
+    amp::array_view<data_type> data_view;
+};
+
+template<typename data_type, typename accessor_type>
+void measure_time(const int32_t size, const uint32_t seed, accessor_type & accessor)
+{
+    std::srand(seed);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for(int i = 0; i < size; ++i) {
+        int pos = std::rand() % size;
+        //std::cout << "Process: " << pos << std::endl;
+        accessor.read_write(pos, std::rand());
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> diff = end-start;
+    std::cout << "Time: " << diff.count() << std::endl;
+}
+
+int main(int argc, char ** argv)
+{
+    assert(argc > 1);
+    const int32_t size = atoi(argv[1]);
+    const uint32_t seed = (unsigned int)std::time(0);
+	amp::accelerator default_acc;
+	amp::accelerator_view acc_view = default_acc.get_default_view();
+	std::wcout << "Using device: " << default_acc.get_description() << std::endl;
+    std::cout << "Using seed: " << seed << std::endl;
+    
+    amp::array<int> device_data(amp::extent<1>(size), acc_view);
+    amp::parallel_for_each(device_data.get_extent(),
+        [=, &device_data](amp::index<1> idx) restrict(amp) {
+            device_data[ idx[0] ] = 0;
+        }
+    );
+    acc_view.wait();
+    
+    {
+        view_accessor<int> acc(device_data);
+        measure_time<int>(size, seed, acc);
+    }
+
+    return 0;
+}
